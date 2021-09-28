40b98f548d0bcf46aed6cc74d0224c3d57d0a0e5
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2021-09-27 23:22:08 -0400

    Clean comments from test

--- palindrome.test.js
+++ palindrome.test.js
@@ -9,18 +9,3 @@ for (const algorithm of testsToRun.algorithms) {
         });
     })
 }
-
-// for (const algorithm of testsToRun.algorithms) {
-//     describe(algorithm.name, () => {
-//         const startTime = Date.now();
-//         for (const testData of testsToRun.tests) {
-//             test(`Output of ${testData.output} from input ${testData.input}`, () => {
-//                 expect(algorithm.funcReference(testData.input)).toBe(testData.output);
-//             })
-//         }
-//         const endTime = Date.now();
-//         test(`Suite completed in ${endTime - startTime}ms`, () => {
-//             expect(true).toBe(true);
-//         });
-//     })
-// }
